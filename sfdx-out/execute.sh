60f365bb918885494ad33a62d914ab11be57ea5f
nishimura <n-atsu-dream@hotmail.co.jp>
2022-01-29 16:18:40 +0900

    commit

--- sfdx-out/execute.sh
+++ sfdx-out/execute.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 本番(エイリアス名: abc)からデータをエクスポート
-sfdx force:data:tree:export -u abc \
---query "SELECT Id, Name, custom_text__c FROM test_custom_obj__c" \
---prefix export-demo \
---outputdir sfdx-out --plan
+sfdx force:data:tree:export \
+ -u abc \
+ -q "SELECT Id, Name, custom_text__c FROM test_custom_obj__c"  \
+ --prefix export --outputdir sfdx-out
 
 # スクラッチ組織(エイリアス名: myproject10)にインポート
 sfdx force:data:tree:import -u myproject10 \
---plan sfdx-out/export-demo-test_custom_obj__c-plan.json
+-f sfdx-out/export-test_custom_obj__c.json
